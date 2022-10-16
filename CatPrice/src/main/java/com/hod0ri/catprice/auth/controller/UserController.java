00fb3951bf74dbf8f0ce750e663b9d37e0fcf34f
Hod0ri <minthod0ri@gmail.com>
2022-10-16 21:05:35 +0900

    [ Spring Boot ] User Registration

--- CatPrice/src/main/java/com/hod0ri/catprice/auth/controller/UserController.java
+++ CatPrice/src/main/java/com/hod0ri/catprice/auth/controller/UserController.java
@@ -8,10 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.security.NoSuchAlgorithmException;
 
 @Slf4j
@@ -32,5 +32,25 @@ public class UserController {
         user.setUser_password(sha256.encrypt(user.getUser_password()));
 
         userService.register(user);
+    }
+
+    @RequestMapping("/signup")
+    public String signupPage(HttpServletRequest request) {
+//        HttpSession session = request.getSession();
+//        if (session == null) {
+//            return "auth/UserLogin";
+//        } else {
+//            //log.info(session.getAttribute("user_id").toString());
+//            return "mg/index";
+//        }
+        return "auth/UserLogin";
+    }
+
+    @GetMapping("/login")
+    public void loginPage(@RequestParam String user_id, @RequestParam String password, HttpServletRequest request) throws NoSuchAlgorithmException {
+        log.info("successful!");
+        log.info("user_id: " + user_id + " password: " + password);
+        //HttpSession session = request.getSession();
+        //session.setAttribute("user_id", user_id);
     }
 }
